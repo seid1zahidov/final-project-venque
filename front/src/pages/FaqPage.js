aa5495b0e762e08d8a080ea5addf4696d54b9e2d
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-09-22 17:19:05 +0400

    hjfijdsihds

--- front/src/pages/FaqPage.js
+++ front/src/pages/FaqPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
+import '../assets/page/faq.css'
 const FaqPage = () => {
 
     const [faq, setFaq] = useState([])
@@ -12,39 +12,37 @@ const FaqPage = () => {
     }, [])
 
     return (
-
         <section className="FAQs">
-            <div className="container">
-                <div className="row">
-                    <div className="subject">
-                        <h1 id='return'>Frequently Asked Questions</h1>
-                        <h4 className='second'>Questions</h4>
-                        <ol>
-                            {faq && faq.map((faqs) => (
-                                <li>
-                                    <a href={`#${faqs.idi}`}>
-                                        {faqs.subject}
-                                    </a>
-                                </li>
-                            ))}
-                        </ol>
-                    </div>
-                    <hr />
-                    <div className="answer">
-                        <h4 className='second mb-3 mt-3'>Answers</h4>
-                        {faq && faq.map((faqs) => (
-                            <div>
-                                <h4 id={`${faqs.id}`} className='question ms-3 mt-3'><span className='me-3 a'>Q:</span>{faqs.author}</h4>
-                                <p className='ms-3'><span className='me-3 a'>A:</span>{faqs.text}</p>
-                                <a href="#return" className='rtrn mt-3 mb-3'>Return to top</a>
-                                <hr />
-                            </div>
+        <div className="container">
+            <div className="row">
+                <div className="subject">
+                    <h1 id='return'>Frequently Asked Questions</h1>
+                     <h4 className='second'>Questions</h4>
+                     <ol>
+                     {faq && faq.map((faqs) => (
+                        <li>
+                            <a href={`#${faqs.idi}`}>
+                                {faqs.author}
+                            </a>
+                        </li>
                         ))}
-                    </div>
+                     </ol>
+                </div>
+                <hr />
+                <div className="answer">
+                    <h4 className='second mb-3 mt-3'>Answers</h4>
+                    {faq && faq.map((faqs) => (
+                       <div>
+                       <h4  id={`${faqs.idi}`} className='question ms-3 mt-3'><span className='me-3 a'>Q:</span>{faqs.author}</h4>
+                       <p className='ms-3'><span className='me-3 a'>A:</span>{faqs.text}</p>
+                       <a href="#return" className='rtrn mt-3 mb-3'>Return to top</a>
+                       <hr />
+                       </div>
+                       ))}
                 </div>
             </div>
-        </section>
-
+        </div>
+    </section>
     )
 }
 
