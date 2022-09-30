840823c518e03a1740e09d5344d4a8fe3cada8d7
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-09-30 21:10:26 +0400

    fdskjbnijdfs

--- front/src/Modal/Shopfilter.js
+++ front/src/Modal/Shopfilter.js
@@ -5,27 +5,88 @@ import './shopfilter.css'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import Filterstart from '../components/Filter/Filterstart';
-
-
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
 
 
 
 const Shopfilter = ({ show, closeModal }) => {
 
+  function valuetext(value) {
+    return `${value}°C`;
+  }
+  const [value, setValue] = React.useState([20, 37]);
 
+  const handleChange = (event, newValue) =>
+    setValue(newValue);
 
-    if (!show) return null
-
+  if (!show) return null
 
   return (
     <div className='Burgerss'>
-    <div className="Shopoverlay" onClick={closeModal}></div>
-    <div className="Shopcontend">
-      <Filterstart />
-        <AiOutlineClose className='Shopburger' onClick={closeModal} />
+      <div className="Shopoverlay" onClick={closeModal}></div>
+      <div className="Shopcontend">
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Availability
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    In stock
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Out of stock
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Price
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <Box className='mt-5' sx={{ width: 200 }}>
+                  <Slider
+                    getAriaLabel={() => 'Temperature range'}
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={valuetext}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    '& > legend': { mt: 2 },
+                  }}
+                >
+                  <Typography component="legend">Controlled</Typography>
+                  <Rating
+                    name="simple-controlled"
+                    onChange={(event, newValue) => {
+                    }}
+                  />
+                </Box>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-</div>
   )
 }
 
--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,45 +1,82 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Header.css'
-import {GiHamburgerMenu} from 'react-icons/gi';
-import {BsPerson} from 'react-icons/bs';
-import {BsBasketFill} from 'react-icons/bs';
-import {AiOutlineSearch} from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { BsPerson } from 'react-icons/bs';
+import { BsBasketFill } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
 import Burger from '../../Modal/Burger';
 import { Link } from 'react-router-dom';
 import { Cart } from '../Cart/Context';
 import Card from '../../Modal/Card';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import axios from 'axios';
+import SingleProduct from '../Cart/SingleProduct';
 
 
 const Header = () => {
-  const [show , setShow] = useState(false)
-  const [carts , CartShow] = useState(false)
-  const {cart} = useContext(Cart)
+  const [showe, setShowe] = useState(false)
+  const [carts, CartShow] = useState(false)
+  const { cart } = useContext(Cart)
+
+  const [search, setSearch] = useState("")
+
+
+  const SearcHandler = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredProducst = cart.filter(card => card.name.toLowerCase().includes(search.toLowerCase()))
+
+  // <AiOutlineSearch className='search  ms-5' />`  
+  const [cartt, setCart] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3002/card')
+      .then(cart => setCart(cart.data.card))
+      .catch(e => console.log(e))
+  }, [])
+  const [visible, setVisible] = useState(12)
+
 
   return (
     <header id='header'>
-          <div className='containers '>
-         <div className="row headerss align-items-center">
-            <div className="col-lg-2 col-4 text-start d-flex">
-                   <Burger  />
-                <AiOutlineSearch className='search  ms-5'/>
+      <div className='containers '>
+        <div className="row headerss align-items-center">
+          <div className="col-lg-2 col-4 text-start d-flex">
+            <Burger />
+            <AiOutlineSearch class="search ms-5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop" />
+            <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+              <div class="offcanvas-header">
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                {filteredProducst && filteredProducst.slice(0, visible).map((cardss) => ((
+                  <SingleProduct prod={cardss} key={cardss._id} />
+                )))}
+                <input onChange={SearcHandler} className="search" type="text" placeholder="search" />
+
+              </div>
             </div>
-            <div className="col-lg-8 col-4 text-center headercenter">
-              <Link to="/" >
-                <img className='text-center' src="https://cdn.shopify.com/s/files/1/1592/8017/files/logo_file_160x.png?v=1540930515" alt="sekil" />
-              </Link>
-            </div>
-            <div className="col-lg-2 col-4 headerright text-end">
-              <Link to='/login'>
+          </div>
+          <div className="col-lg-8 col-4 text-center headercenter">
+            <Link to="/" >
+              <img className='text-center' src="https://cdn.shopify.com/s/files/1/1592/8017/files/logo_file_160x.png?v=1540930515" alt="sekil" />
+            </Link>
+          </div>
+          <div className="col-lg-2 col-4 headerright text-end">
+            <Link to='/login'>
               <BsPerson className='person' />
-              </Link>
-                <BsBasketFill className='righticon' onClick={() => CartShow((s) => !s)}/>
-                   <Card  show={carts} closeModal ={() => CartShow(false)} />
-                  ({cart.length})
-            </div>
-         </div>
-    </div>
+            </Link>
+            <BsBasketFill className='righticon' onClick={() => CartShow((s) => !s)} />
+            <Card show={carts} closeModal={() => CartShow(false)} />
+            ({cart.length})
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
+
+
 
 export default Header
