d12599443692fb36dbfe907edd45dc7fb3dab856
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-10-06 01:53:55 +0400

    dfsnnfsonf

--- front/src/pages/SingleProd.jsx
+++ front/src/pages/SingleProd.jsx
@@ -20,8 +20,8 @@ function reducer(state, action) {
     }
 }
 
-const SingleProd = ( {prod}) => {
-  const { cart, setCart } = useContext(Cart)
+const SingleProd = ({ prod }) => {
+    const { cart, setCart } = useContext(Cart)
 
 
     const [{ loading, singleproduct }, dispatch] = useReducer(reducer, {
@@ -74,12 +74,86 @@ const SingleProd = ( {prod}) => {
                             <option value="">9</option>
                             <option value="">10</option>
                         </select> <br />
-                            <button class="glow-on-hover cart_single" onClick={() => { setCart([singleproduct]) }} type="button">Add to cart</button>
-                            <button class="glow-on-hover cart_singles"  type="button">Buy it now</button>
-                            <img className='detailsimg' src="https://cdn.shopify.com/s/files/1/1592/8017/files/website-icon-wiget_d7191648-b959-4125-9ce7-308d4b389425.jpg?v=1650296931" alt="" />
+                        <button class="glow-on-hover cart_single" onClick={() => { setCart([singleproduct]) }} type="button">Add to cart</button>
+                        <button class="glow-on-hover cart_singles" type="button">Buy it now</button>
+                        <img className='detailsimg' src="https://cdn.shopify.com/s/files/1/1592/8017/files/website-icon-wiget_d7191648-b959-4125-9ce7-308d4b389425.jpg?v=1650296931" alt="" />
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                            <div class="accordion-items">
+                                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                        Description
+                                    </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                    <div class="accordion-body">
+                                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-items">
+                                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                        Features
+                                    </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                                    <div class="accordion-body">
+                                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-items">
+                                <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                                        Specifications
+                                    </button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                                    <div class="accordion-body">
+                                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
-                
+                <section>
+                    <img className='cardteails_bodyimg ' src="https://cdn.shopify.com/s/files/1/1592/8017/files/Transit-details_1950x.jpg?v=1658434604" alt="" />
+                </section>
+                <section id='detailscarusel'>
+                    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://cdn.shopify.com/s/files/1/1592/8017/files/download_4_1950x.jpg?v=1648572162" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://cdn.shopify.com/s/files/1/1592/8017/files/download_2_1950x.jpg?v=1648572053" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://cdn.shopify.com/s/files/1/1592/8017/files/download_2_1950x.jpg?v=1648572053" class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </section>
+                <section id='details-tree_section'>
+                    <div className='d-flex'>
+                        <div className="col-lg-6">
+                            <img src="https://cdn.shopify.com/s/files/1/1592/8017/files/Transit-web1_1950x.jpg?v=1650562879" alt="" />
+                        </div>
+                        <div className="col-lg-6">
+                            <img src="https://cdn.shopify.com/s/files/1/1592/8017/files/Transit-web02_1950x.jpg?v=1650562950" alt="" />
+                        </div>
+                    </div>
+                </section>
+
             </section>
         </>
     )
